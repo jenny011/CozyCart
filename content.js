84e225661c8bea62ab014749d8881f89c94f8b4a
Jenny Zhu <jz2915@nyu.edu>
2020-07-26 22:14:42 +0800

    update

--- content.js
+++ content.js
@@ -1,12 +1,23 @@
+var keyword;
+
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.executeScript({
         code: "window.getSelection().toString(); "
     }, function(selection) {
+    	keyword = selection[0];
         chrome.tabs.create({
             //url: "https://www.amazon.com/s?k=" + selection[0] + "&i=amazonfresh" 
             url: "./products.html"
         });
     });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.poke == "get product links") {
+		sendResponse({product: keyword, links: [{name: "1", content: "1"}]})
+	} else {
+		sendResponse("Don't understand the request");
+	}
 });
 
 // function getSelection() {
